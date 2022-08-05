589719cd4a66d2a605ed6a0b47d36f3fc7b63836
tomigarcia <tomigarcia89@gmail.com>
2022-08-05 20:19:00 -0300

    Resolviendo HM CS

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,11 +2,25 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let sumaTotal = 0
+  for(let i = 0; i < num.length ; i++) {
+    sumaTotal= sumaTotal + Math.pow(2 , num.length - i - 1) * num[i]
 
+  }
+  return sumaTotal;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
+  //div el numero hasta llegar a 0
+  let result = [] 
+  while (num > 0) {
+    result.push(num%2);
+    num = Math.floor(num/2);
+  }
+  
+  return result.reverse().join('');
+
+
 
 }
 
